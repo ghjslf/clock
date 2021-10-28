ba05ca1c96aa8b8ba2c4a018970bd8c0f22c0d78
Pashkov <pashkov@example.com>
2021-10-28 11:53:28 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function convert() {
+    let time = new Date();
+    let hours = time.getHours().toString();
+    let minutes = time.getMinutes().toString();
+    let seconds = time.getSeconds().toString();
+
+    if (hours.length == 1) {
+        hours = '0' + hours;
+    }
+    if (minutes.length == 1) {
+        minutes = '0' + minutes;
+    }
+    if (seconds.length == 1) {
+        seconds = '0' + seconds;
+    }
+
+    let clockString = hours + ':' + minutes + ':' + seconds;
+    let colorString = '#' + hours + minutes + seconds;
+
+    clock.textContent = clockString;
+    color.textContent = colorString;
+
+    document.body.style.background = colorString;
+}
+
+
+let clock = document.getElementById('clock');
+let color = document.getElementById('color');
+
+convert();
+setInterval(convert, 1000);
